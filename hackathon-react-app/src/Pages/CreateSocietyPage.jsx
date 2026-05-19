7a563633eea1c9dbbb67a50b968ea75b363fbeda
oliver <oliverluan04@gmail.com>
2026-05-19 22:40:45 +1000

    added page for creating society page

---
+++ hackathon-react-app/src/Pages/CreateSocietyPage.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import { Routes, Route, Link, Navigate, useNavigate} from "react-router-dom"
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+
+function CreateSocietyPage() {
+
+
+    return (
+        <div>
+            Test
+
+        </div>
+    )
+}
+
+export default CreateSocietyPage
