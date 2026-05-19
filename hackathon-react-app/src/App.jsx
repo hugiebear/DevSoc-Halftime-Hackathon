c23f7b5e8df37a2a3e0b2a148aa531a347625f27
Tristan Tran <z5584506@ad.unsw.edu.au>
2026-05-19 23:44:18 +1000

    added profile url

--- hackathon-react-app/src/App.jsx
+++ hackathon-react-app/src/App.jsx
@@ -1,84 +1,87 @@
-import { useState, useEffect } from 'react'
-import { Routes, Route, Link, Navigate, useNavigate} from "react-router-dom"
-import './App.css'
-import EventListings from './EventListings'
-import Login from './Pages/Login'
-import Register from './Pages/Register'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Toolbar from '@mui/material/Toolbar'
+import { useEffect, useState } from 'react'
+import { Link, Route, Routes, useNavigate } from "react-router-dom"
+import './App.css'
+import EventListings from './EventListings'
 import CreateSocietyPage from './Pages/CreateSocietyPage'
+import Login from './Pages/Login'
+import Profile from './Pages/Profile'
+import Register from './Pages/Register'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [token, setToken] = useState(null);
+    const [count, setCount] = useState(0)
+    const [token, setToken] = useState(null);
 
-  useEffect(() => {
-    const localToken = localStorage.getItem('token');
-    setToken("test");
-  }, []);
+    useEffect(() => {
+        const localToken = localStorage.getItem('token');
+        setToken("test");
+    }, []);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  function fn(token) {
-    localStorage.setItem('token', token);
-    setToken(token);
-    navigate('/events')
-    console.log("redirect");
-  }
+    function fn(token) {
+        localStorage.setItem('token', token);
+        setToken(token);
+        navigate('/events')
+        console.log("redirect");
+    }
 
-  const confirmLogout = () => {
-    setToken(null);
-    navigate('/login');
-  }
+    const confirmLogout = () => {
+        setToken(null);
+        navigate('/login');
+    }
 
-  return (
-   <div>
-    {token ? (
-      <>
-      <nav>
-        <Box>
-          <AppBar position='static'>
-            <Toolbar sx={{ display: "flex", justifyContent: "left", gap: 5}}>
-              <Link to="/events">View Upcoming Events</Link>
-              <Link to="/createsocietypage">Create a page for your society!</Link>
+    return (
+        <div>
+            {token ? (
+                <>
+                    <nav>
+                        <Box>
+                            <AppBar position='static'>
+                                <Toolbar sx={{ display: "flex", justifyContent: "left", gap: 5 }}>
+                                    <Link to="/events">View Upcoming Events</Link>
+                                    <Link to="/createsocietypage">Create a page for your society!</Link>
 
-              <Button sx={{ ml: "auto"}} variant="contained" color="error" onClick={confirmLogout}>
-                Logout
-              </Button>
-            </Toolbar>
-          </AppBar>
-        </Box>
-      </nav>      
-      </>
-    ) : (
-      <Box>
-          <AppBar position='static'>
-            <Toolbar sx={{ display: "flex", justifyContent: "left", gap: 5}}>
-              <Link to="/login">Login here!</Link>
-              <Link to="/register">Register here!</Link>
-            </Toolbar>
-          </AppBar>
+                                    <Link to="/profile/:id">View Profile</Link>
+                                    <Button sx={{ ml: "auto" }} variant="contained" color="error" onClick={confirmLogout}>
+                                        Logout
+                                    </Button>
+                                </Toolbar>
+                            </AppBar>
+                        </Box>
+                    </nav>
+                </>
+            ) : (
+                <Box>
+                    <AppBar position='static'>
+                        <Toolbar sx={{ display: "flex", justifyContent: "left", gap: 5 }}>
+                            <Link to="/login">Login here!</Link>
+                            <Link to="/register">Register here!</Link>
+                        </Toolbar>
+                    </AppBar>
 
-      </Box>
-    )}
+                </Box>
+            )}
 
 
-    
-    <Routes>
-      <Route path="/login" element={<Login successCallback={fn}/>} />
-      <Route path="/register" element={<Register successCallback={fn}/>} />
-      <Route
-        path="/events"
-        element={<EventListings/>}
-      />
-      <Route path="/createsocietypage" element={<CreateSocietyPage/>}/>
-      
-    </Routes>
-   </div>
-   
-  )
+
+            <Routes>
+                <Route path="/login" element={<Login successCallback={fn} />} />
+                <Route path="/register" element={<Register successCallback={fn} />} />
+                <Route
+                    path="/events"
+                    element={<EventListings />}
+                />
+                <Route path="/createsocietypage" element={<CreateSocietyPage />} />
+                <Route path="/profile/:id" element={<Profile />} />
+
+            </Routes>
+        </div>
+
+    )
 }
 
 export default App
